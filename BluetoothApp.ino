23549b9c959f632e6a1c75ebaac76377ec51e0a5
JuanEspinosa <juan.espinosa2@ge.com>
2016-05-03 12:08:28 -0400

    Minor formatting

--- BluetoothApp.ino
+++ BluetoothApp.ino
@@ -336,10 +336,6 @@ void setup()
 
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
-//  while (!Serial) {
-//    ; // wait for serial port to connect. Needed for Leonardo only
-//  }
-
   Serial.println("Goodnight moon!");
 
   // set the data rate for the SoftwareSerial port
